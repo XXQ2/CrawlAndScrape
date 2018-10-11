fa0d2d14ee84e465a4e68a7c888d1daa1508c45b
頼本圭太 <asbo0502@gmail.com>
2018-10-11 23:16:05 +0900

    データセットの取得と活用

---
+++ chapter_5/1.mecab_sample.py
@@ -0,0 +1,14 @@
+# MeCabをPythonから使う
+
+import MeCab
+
+tagger = MeCab.Tagger()
+tagger.parse('')  # これは .parseToNode()の不具合を回避するために必要
+
+# parseToNodeで最初の形態素を表すNodeオブジェクトを取得する
+node = tagger.parseToNode('すもももももももものうち')
+
+while node:
+    # .surfaceは形態素の文字列、.featureは品詞などを含む文字列をそれぞれ表す
+    print(node.surface, node.feature)
+    node = node.next  # .nextで次のNodeを取得する
